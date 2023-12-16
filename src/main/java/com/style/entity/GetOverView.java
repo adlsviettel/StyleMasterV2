fd86fdc9ee89a7c45c249e0b17c74c5fe72c1fa4
DESKTOP-APOKGHJ\MSI <tnmrtupro1@gmail.com>
2023-12-16 11:24:35 +0700

    Initial commit

--- src/main/java/com/style/entity/GetOverView.java
+++ src/main/java/com/style/entity/GetOverView.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -48,6 +50,9 @@ public class GetOverView {
 
     @Column(name = "Status")
     private int status;
+
+    @Column(name = "CreatedDate")
+    private Date createdDate;
 
     @Column(name = "TotalRowNum")
     private int totalRowNum;
