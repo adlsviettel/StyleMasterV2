739d35eb77fe59c60edcfc3abc6449271082f18b
DESKTOP-APOKGHJ\MSI <tnmrtupro1@gmail.com>
2023-12-18 07:46:35 +0700

    Initial commit

--- src/main/java/com/style/repository/SearchOverViewRepository.java
+++ src/main/java/com/style/repository/SearchOverViewRepository.java
@@ -1,7 +1,6 @@
 package com.style.repository;
 
 import com.style.entity.GetOverView;
-import com.style.entity.SearchOverView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
